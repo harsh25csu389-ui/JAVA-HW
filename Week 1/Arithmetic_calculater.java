bccb2c82c96cafb46c5dee196db3e1d63c046db8
gamevintager <138240515+gamevintager@users.noreply.github.com>
2026-08-04 11:28:32 +0530

    changes

--- Week 1/Arithmetic_calculater.java
+++ Week 1/Arithmetic_calculater.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class studentinput{
+public class Arithmetic_calculater{
     public static void main(String[] args){
         
         Scanner input = new Scanner(System.in);
